ddef6c160a76b09fd9e0f47ec056902676b1292b
Renxoll <112541849+Renxoll@users.noreply.github.com>
2026-02-19 23:36:09 -0500

    feat: change endpoints name for login

--- src/services/authService.js
+++ src/services/authService.js
@@ -7,18 +7,19 @@ const roleMapping = {
 };
 
 export const login = async (email, password) => {
-  // 1. Crear los datos como formulario web estándar (URLSearchParams)
-  // Esto es OBLIGATORIO para que FastAPI OAuth2 lo acepte
   const params = new URLSearchParams();
   params.append('username', email);
   params.append('password', password);
 
-  // 2. Enviar con la cabecera explícita
-  const response = await api.post('/auth/auth/login', params, {
+  const response = await api.post('/auth/login', params, {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     }
   });
+
+  if (response.data.access_token) {
+    localStorage.setItem('token', response.data.access_token);
+  }
 
   return response.data;
 };
